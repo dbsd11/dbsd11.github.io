c1e9c1bdc989ccc15443b6c8f22824c408bb5b95
diaobisong <diaobisong@wecash.net>
2016-11-21 19:19:27 +0800

    clean

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-  oldgo=$GOPATH
-  GOPATH=$(cd `dirname $0`; pwd)
-  GOBIN=$GOPATH/bin
-
-  go run $1
-
-  GOPATH=$oldgo
-  GOBIN=$GOPATH/bin
--- stop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-  kill -9 $(ps -ax|grep go|grep -v grep|cut -c 1-5)
